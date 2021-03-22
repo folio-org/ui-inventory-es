4b729608cd66144eaeda35c5abcb00fa5046990e
Dmytro-Melnyshyn <Dmytro_Melnyshyn@epam.com>
2021-03-22 19:58:28 +0200

    make changes to the 'holdings' segment

--- src/components/HoldingsRecordFilters/HoldingsRecordFilters.js
+++ src/components/HoldingsRecordFilters/HoldingsRecordFilters.js
@@ -6,118 +6,119 @@ import {
   Accordion,
   FilterAccordionHeader,
 } from '@folio/stripes/components';
+import { AccordionSet } from '@folio/stripes-components';
 
 import TagsFilter from '../TagsFilter';
 import CheckboxFacet from '../CheckboxFacet';
+import { useFacets } from '../../common/hooks';
+import { FACETS } from '../../constants';
 
-export default class HoldingsRecordFilters extends React.Component {
-  static propTypes = {
-    activeFilters: PropTypes.objectOf(PropTypes.array),
-    onChange: PropTypes.func.isRequired,
-    onClear: PropTypes.func.isRequired,
-    data: PropTypes.object,
-  };
-
-  static defaultProps = {
-    activeFilters: {},
-    data: {
-      locations: [],
+const HoldingsRecordFilters = (props) => {
+  const {
+    activeFilters: {
+      holdingsDiscoverySuppress,
+      effectiveLocation,
+      holdingsPermanentLocation,
+      holdingsTags,
     },
-  }
+    data,
+    onChange,
+    onClear,
+  } = props;
 
-  render() {
-    const {
-      activeFilters: {
-        discoverySuppress = [],
-        effectiveLocation = [],
-        holdingsPermanentLocation = [],
-        tags,
-      },
-      data: {
-        locations,
-        tagsRecords,
-      },
-      onChange,
-      onClear,
-    } = this.props;
+  const [
+    accordions,
+    onToggleSection,
+    handleFetchFacets,
+    handleFilterSearch,
+    facetsOptions,
+    getIsPending,
+  ] = useFacets(props.activeFilters, data);
 
-    const locationOptions = locations.map(({ name, id }) => ({
-      label: name,
-      value: id,
-    }));
-
-    const suppressedOptions = [
-      {
-        label: <FormattedMessage id="ui-inventory.yes" />,
-        value: 'true',
-      },
-      {
-        label: <FormattedMessage id="ui-inventory.no" />,
-        value: 'false',
-      },
-    ];
-
-    return (
-      <>
-        <Accordion
-          label={<FormattedMessage id="ui-inventory.filters.effectiveLocation" />}
-          id="holdingsEffectiveLocationAccordion"
-          name="effectiveLocation"
-          separator={false}
-          header={FilterAccordionHeader}
-          displayClearButton={effectiveLocation.length > 0}
-          onClearFilter={() => onClear('effectiveLocation')}
-        >
-          <CheckboxFacet
-            name="effectiveLocation"
-            dataOptions={locationOptions}
-            selectedValues={effectiveLocation}
-            onChange={onChange}
-            isFilterable
-          />
-        </Accordion>
-        <Accordion
-          label={<FormattedMessage id="ui-inventory.holdings.permanentLocation" />}
-          id="holdingsPermanentLocationAccordion"
-          name="holdingsPermanentLocation"
-          closedByDefault
-          header={FilterAccordionHeader}
-          displayClearButton={holdingsPermanentLocation.length > 0}
-          onClearFilter={() => onClear('holdingsPermanentLocation')}
-        >
-          <CheckboxFacet
-            name="holdingsPermanentLocation"
-            dataOptions={locationOptions}
-            selectedValues={holdingsPermanentLocation}
-            onChange={onChange}
-            isFilterable
-          />
-        </Accordion>
-        <Accordion
-          data-test-filter-holding-discovery-suppress
-          label={<FormattedMessage id="ui-inventory.discoverySuppress" />}
-          id="holdingDiscoverySuppress"
-          name="discoverySuppress"
-          closedByDefault
-          header={FilterAccordionHeader}
-          displayClearButton={discoverySuppress.length > 0}
-          onClearFilter={() => onClear('discoverySuppress')}
-        >
-          <CheckboxFacet
-            data-test-filter-holdings-discovery-suppress
-            name="discoverySuppress"
-            dataOptions={suppressedOptions}
-            selectedValues={discoverySuppress}
-            onChange={onChange}
-          />
-        </Accordion>
-        <TagsFilter
+  return (
+    <AccordionSet accordionStatus={accordions} onToggle={onToggleSection}>
+      <Accordion
+        label={<FormattedMessage id={`ui-inventory.filters.${FACETS.EFFECTIVE_LOCATION}`} />}
+        id={FACETS.EFFECTIVE_LOCATION}
+        name={FACETS.EFFECTIVE_LOCATION}
+        separator={false}
+        header={FilterAccordionHeader}
+        displayClearButton={effectiveLocation?.length > 0}
+        onClearFilter={() => onClear(FACETS.EFFECTIVE_LOCATION)}
+      >
+        <CheckboxFacet
+          name={FACETS.EFFECTIVE_LOCATION}
+          dataOptions={facetsOptions.effectiveLocationOptions}
+          selectedValues={effectiveLocation}
           onChange={onChange}
-          onClear={onClear}
-          selectedValues={tags}
-          tagsRecords={tagsRecords}
+          onFetch={handleFetchFacets}
+          onSearch={handleFilterSearch}
+          isPending={getIsPending(FACETS.EFFECTIVE_LOCATION)}
+          isFilterable
         />
-      </>
-    );
-  }
-}
+      </Accordion>
+      <Accordion
+        label={<FormattedMessage id="ui-inventory.holdings.permanentLocation" />}
+        id={FACETS.HOLDINGS_PERMANENT_LOCATION}
+        name={FACETS.HOLDINGS_PERMANENT_LOCATION}
+        closedByDefault
+        header={FilterAccordionHeader}
+        displayClearButton={holdingsPermanentLocation?.length > 0}
+        onClearFilter={() => onClear(FACETS.HOLDINGS_PERMANENT_LOCATION)}
+      >
+        <CheckboxFacet
+          name={FACETS.HOLDINGS_PERMANENT_LOCATION}
+          dataOptions={facetsOptions.holdingsPermanentLocationOptions}
+          selectedValues={holdingsPermanentLocation}
+          onChange={onChange}
+          onFetch={handleFetchFacets}
+          onSearch={handleFilterSearch}
+          isPending={getIsPending(FACETS.HOLDINGS_PERMANENT_LOCATION)}
+          isFilterable
+        />
+      </Accordion>
+      <Accordion
+        data-test-filter-holding-discovery-suppress
+        label={<FormattedMessage id="ui-inventory.discoverySuppress" />}
+        id={FACETS.HOLDINGS_DISCOVERY_SUPPRESS}
+        name={FACETS.HOLDINGS_DISCOVERY_SUPPRESS}
+        closedByDefault
+        header={FilterAccordionHeader}
+        displayClearButton={holdingsDiscoverySuppress?.length > 0}
+        onClearFilter={() => onClear(FACETS.HOLDINGS_DISCOVERY_SUPPRESS)}
+      >
+        <CheckboxFacet
+          data-test-filter-holdings-discovery-suppress
+          name={FACETS.HOLDINGS_DISCOVERY_SUPPRESS}
+          dataOptions={facetsOptions.discoverySuppressOptions}
+          isPending={getIsPending(FACETS.HOLDINGS_DISCOVERY_SUPPRESS)}
+          selectedValues={holdingsDiscoverySuppress}
+          onChange={onChange}
+        />
+      </Accordion>
+      <TagsFilter
+        id={FACETS.HOLDINGS_TAGS}
+        onChange={onChange}
+        onFetch={handleFetchFacets}
+        onSearch={handleFilterSearch}
+        onClear={onClear}
+        selectedValues={holdingsTags}
+        tagsRecords={facetsOptions.tagsRecords}
+        isPending={getIsPending(FACETS.HOLDINGS_TAGS)}
+      />
+    </AccordionSet>
+  );
+};
+
+HoldingsRecordFilters.propTypes = {
+  activeFilters: PropTypes.objectOf(PropTypes.array),
+  onChange: PropTypes.func.isRequired,
+  onClear: PropTypes.func.isRequired,
+  data: PropTypes.object,
+};
+
+HoldingsRecordFilters.defaultProps = {
+  activeFilters: {},
+};
+
+export default HoldingsRecordFilters;
